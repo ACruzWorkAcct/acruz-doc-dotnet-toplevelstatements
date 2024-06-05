36e02375cfdf8b0ae5606054b2cb7cc4b4d4e801
Alejandro Cruz <acruz.workacct@outlook.com>
2024-06-05 16:10:04 -0400

    Code comment

--- TopLevelFunctions/Program.cs
+++ TopLevelFunctions/Program.cs
@@ -4,5 +4,6 @@ WhatsMyNamespace();
 
 void WhatsMyNamespace()
 {
+    // Validate code in default "null" namespace
     WriteLine("Namespace of Program class: {0}", arg0: typeof(Program).Namespace ?? "null");
 }
